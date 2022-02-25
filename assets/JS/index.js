337202c0aa734e0f2bf70d554276dac8cdb60c24
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-02-25 16:09:26 +0200

    Init commit

---
+++ assets/JS/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// 1)
+function getValue() {
+    const link = document.getElementById("w3r");
+
+    const href = link.getAttribute('href');
+    const hreflang = link.getAttribute('hreflang');
+    const rel = link.getAttribute('rel');
+    const target = link.getAttribute('target');
+    const type = link.getAttribute('type');
+
+    const result = [href, hreflang, rel, target, type];
+    return result;
+}
+
+const funcResult = getValue();
+
+console.log(funcResult);
